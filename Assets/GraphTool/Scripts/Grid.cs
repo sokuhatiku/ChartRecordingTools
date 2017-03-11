8d677f8433542ca3f72f32b6b7f8365506bbdf6f
sokuhatiku <takedutu@gmail.com>
2017-03-12 00:21:40 +0900

    grid draws by shader

--- Assets/GraphTool/Scripts/Grid.cs
+++ Assets/GraphTool/Scripts/Grid.cs
@@ -23,6 +23,7 @@ namespace GraphTool
 		public float subGridRadius = 0.5f;
 		public Color subGridColor = new Color(1,1,1,0.5f);
 
+		int sid_Color=0, sid_SubColor=0, sid_Offset=0, sid_Main=0, sid_Sub=0;
 
 #if UNITY_EDITOR
 		protected override void OnValidate()
@@ -31,79 +32,121 @@ namespace GraphTool
 		}
 #endif
 
-		protected override void OnPopulateMesh(VertexHelper vh)
+		protected override void OnEnable()
 		{
-			if (handler == null) return;
-
-			vh.Clear();
-			DrawGrid(vh, true);
-			DrawGrid(vh, false);
-
-		}
-
-		void DrawGrid(VertexHelper vh, bool direction)
-		{
-			var gridsize = direction ?
-				handler.GridScale.x :
-				handler.GridScale.y;
-			if (gridsize < 0.01f) return;
-
-			var subdivision = direction ?
-				handler.GridSubdivisionX :
-				handler.GridSubdivisionY;
-			if (subdivision < 1) return;
-
-			var width = direction ?
-				handler.ScopeRect.width :
-				handler.ScopeRect.height;
-
-			var start = direction ?
-				handler.ScopeRect.xMin :
-				handler.ScopeRect.yMin;
+			base.OnEnable();
 			
+			var shader = Shader.Find("GraphTool/Grid");
+			if (shader) material = new Material(shader);
 
-			var draws = Mathf.CeilToInt(width / gridsize) + 1;
-			while (draws > handler.GridAutoScalingThresholdX)
-			{ gridsize *= subdivision; draws = Mathf.CeilToInt(draws / subdivision); }
-			draws += 2;
-
-
-			// split to subgrid
-			draws *= subdivision;
-			gridsize /= subdivision;
-
-			//if(direction)Debug.Log(start / gridsize);
-			var startNum = Mathf.FloorToInt(start / gridsize);
-			var offset = -(start % gridsize);
-			if (offset > 0) offset -= gridsize; // Graph shape to "/|/|/|/|/|"
-
-
-			// convert to rectTransform position
-			var tf_set = direction ?
-				ScopeToRectX(start + offset) :
-				ScopeToRectY(start + offset);
-			var tf_gain = direction ?
-				gridsize * scale.x :
-				gridsize * scale.y;
-
-			// draw
-			for (int i = 0; i < draws; ++i)
-			{
-				var isMain = (startNum + i) % subdivision == 0;
-				var translated = tf_set + tf_gain * i;
-				if (direction ?
-					translated < rectTransform.rect.xMin - 1 || rectTransform.rect.xMax + 1 < translated :
-					translated < rectTransform.rect.yMin - 1 || rectTransform.rect.yMax + 1 < translated) continue;
-
-				var from = direction ?
-					new Vector3(translated, rectTransform.rect.yMin) :
-					new Vector3(rectTransform.rect.xMin, translated);
-				var to = direction ?
-					new Vector3(translated, rectTransform.rect.yMax) :
-					new Vector3(rectTransform.rect.xMax, translated);
-
-				AddLine(vh, from, to, isMain ? GridRadius : subGridRadius, isMain ? color : subGridColor);
-			}
+			sid_Color = Shader.PropertyToID("_Color");
+			sid_SubColor = Shader.PropertyToID("_SubColor");
+			sid_Offset = Shader.PropertyToID("_Offset");
+			sid_Main = Shader.PropertyToID("_Main");
+			sid_Sub = Shader.PropertyToID("_Sub");
 		}
+
+		protected override void UpdateGraph()
+		{
+			if (!material) return;
+			base.UpdateGraph();
+
+			var scope = handler.ScopeRect;
+			var port = rectTransform.rect;
+			var offset = new Vector4(scope.x / scope.width, scope.y / scope.height, 0, 0);
+
+			var division = new Vector2(scope.size.x /  handler.GridScale.x, scope.size.y / handler.GridScale.y);
+			var subdivision = new Vector2(division.x * handler.GridSubdivisionX, division.y * handler.GridSubdivisionY);
+
+			var main = new Vector4(
+				GridRadius * division.x / port.width,
+				GridRadius * division.y / port.height,
+				division.x, division.y);
+			var sub = new Vector4(
+				subGridRadius * subdivision.x / port.width, 
+				subGridRadius * subdivision.y  / port.height,
+				subdivision.x, subdivision.y);
+
+			material.SetVector(sid_Color, color);
+			material.SetVector(sid_SubColor, subGridColor);
+			material.SetVector(sid_Offset, offset);
+			material.SetVector(sid_Main, main);
+			material.SetVector(sid_Sub, sub);
+		}
+
+		//protected override void OnPopulateMesh(VertexHelper vh)
+		//{
+		//	if (handler == null) return;
+
+		//	//vh.Clear();
+		//	//DrawGrid(vh, true);
+		//	//DrawGrid(vh, false);
+
+		//}
+
+		//void DrawGrid(VertexHelper vh, bool direction)
+		//{
+		//	var gridsize = direction ?
+		//		handler.GridScale.x :
+		//		handler.GridScale.y;
+		//	if (gridsize < 0.01f) return;
+
+		//	var subdivision = direction ?
+		//		handler.GridSubdivisionX :
+		//		handler.GridSubdivisionY;
+		//	if (subdivision < 1) return;
+
+		//	var width = direction ?
+		//		handler.ScopeRect.width :
+		//		handler.ScopeRect.height;
+
+		//	var start = direction ?
+		//		handler.ScopeRect.xMin :
+		//		handler.ScopeRect.yMin;
+
+
+		//	var draws = Mathf.CeilToInt(width / gridsize) + 1;
+		//	while (draws > handler.GridAutoScalingThresholdX)
+		//	{ gridsize *= subdivision; draws = Mathf.CeilToInt(draws / subdivision); }
+		//	draws += 2;
+
+
+		//	// split to subgrid
+		//	draws *= subdivision;
+		//	gridsize /= subdivision;
+
+		//	//if(direction)Debug.Log(start / gridsize);
+		//	var startNum = Mathf.FloorToInt(start / gridsize);
+		//	var offset = -(start % gridsize);
+		//	if (offset > 0) offset -= gridsize; // Graph shape to "/|/|/|/|/|"
+
+
+		//	// convert to rectTransform position
+		//	var tf_set = direction ?
+		//		ScopeToRectX(start + offset) :
+		//		ScopeToRectY(start + offset);
+		//	var tf_gain = direction ?
+		//		gridsize * scale.x :
+		//		gridsize * scale.y;
+
+		//	// draw
+		//	for (int i = 0; i < draws; ++i)
+		//	{
+		//		var isMain = (startNum + i) % subdivision == 0;
+		//		var translated = tf_set + tf_gain * i;
+		//		if (direction ?
+		//			translated < rectTransform.rect.xMin - 1 || rectTransform.rect.xMax + 1 < translated :
+		//			translated < rectTransform.rect.yMin - 1 || rectTransform.rect.yMax + 1 < translated) continue;
+
+		//		var from = direction ?
+		//			new Vector3(translated, rectTransform.rect.yMin) :
+		//			new Vector3(rectTransform.rect.xMin, translated);
+		//		var to = direction ?
+		//			new Vector3(translated, rectTransform.rect.yMax) :
+		//			new Vector3(rectTransform.rect.xMax, translated);
+
+		//		AddLine(vh, from, to, isMain ? GridRadius : subGridRadius, isMain ? color : subGridColor);
+		//	}
+		//}
 	}
 }
---
+++ Assets/GraphTool/Shaders/Grid.shader
@@ -0,0 +1,79 @@
+﻿Shader "GraphTool/Grid" {
+	Properties{
+		_Color("Color", Color) = (1,1,1,1)
+		_SubColor("SubColor", Color) = (1,1,1,0.5)
+		_Offset("Offset", Vector) = (0,0,0,0)
+		_Main("Main", Vector) = (0.1,0.1,1,1) // Size, Division
+		_Sub("Sub", Vector) = (0.05,0.05,10,10) // Size, Division
+	}
+	SubShader
+	{
+		Tags
+		{
+			"Queue" = "Transparent"
+			"IgnoreProjector" = "True"
+			"RenderType" = "Transparent"
+			"PreviewType" = "Plane"
+			"CanUseSpriteAtlas" = "True"
+		}
+
+		Cull Off
+		Lighting Off
+		ZWrite Off
+		ZTest[unity_GUIZTestMode]
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		Pass // sub grid
+		{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			#pragma target 3.0
+			#include "UnityCG.cginc"
+
+			uniform fixed4 _SubColor;
+			uniform fixed4 _Offset;
+			uniform fixed4 _Sub;
+
+			fixed4 frag(v2f_img i) : COLOR
+			{
+				fixed x = frac((i.uv.x + _Offset.x) * _Sub.z - _Sub.x / 2) + _Sub.x;
+				fixed y = frac((i.uv.y + _Offset.y) * _Sub.w - _Sub.y / 2) + _Sub.y;
+
+				clip(max(x, y) - 1);
+				return _SubColor;
+			}
+
+			ENDCG
+		}
+
+		Pass // main grid
+		{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+			#pragma target 3.0
+			#include "UnityCG.cginc"
+
+			uniform fixed4 _Color;
+			uniform fixed4 _Offset;
+			uniform fixed4 _Main;
+
+			fixed4 frag(v2f_img i) : COLOR
+			{
+				fixed x = frac((i.uv.x + _Offset.x) * _Main.z - _Main.x / 2) + _Main.x;
+				fixed y = frac((i.uv.y + _Offset.y) * _Main.w - _Main.y / 2) + _Main.y;
+
+				clip(max(x, y) - 1);
+				return _Color;
+			}
+
+			ENDCG
+		}
+	}
+
+	SubShader
+	{
+		Pass{ Color(0,0,0,1) }
+	}
+}
